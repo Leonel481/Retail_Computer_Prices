cd3ced09b5526d8e3d101470dcd96decbb83bfed
Leonel481 <leonel.aliaga.v@gmail.com>
2023-10-29 18:07:48 -0500

    bd cargada

--- AppCPS/env/products/admin.py
+++ AppCPS/env/products/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import product
+from .resources import MyModelResource
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-admin.site.register(product)
+class MyModelAdmin(ImportExportModelAdmin):
+    resource_class = MyModelResource
+
+admin.site.register(product, MyModelAdmin)
--- AppCPS/env/products/models.py
+++ AppCPS/env/products/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class product(models.Model):
-    titles = models.CharField(max_length = 200 )
+    titles = models.CharField(max_length = 200)
     links = models.URLField(null=True)
     codes = models.CharField(max_length= 10)
-    stocks = models.IntegerField()
-    ratings = models.PositiveIntegerField()
+    stocks = models.IntegerField(null=True)
+    ratings = models.PositiveIntegerField(null=True)
     prices_usd = models.DecimalField(decimal_places= 2, max_digits=10)
     prices_pen = models.DecimalField(decimal_places= 2, max_digits=10)
     images = models.URLField(null=True)
     Fecha = models.DateTimeField(null=True, blank=True)
-    categoria = models.CharField(max_length= 10)
-    empresa = models.CharField(max_length= 10)
-    marca = models.CharField(max_length= 10)
+    categoria = models.CharField(max_length= 50)
+    empresa = models.CharField(max_length= 50)
+    marca = models.CharField(max_length= 50)
