64b63f66cfc4bfbc0e43db187f84e78d64824c9a
Leonel481 <leonel.aliaga.v@gmail.com>
2023-10-29 15:44:43 -0500

    cambios en modelo

---
+++ AppCPS/env/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class product(models.Model):
+    titles = models.CharField(max_length = 200 )
+    links = models.URLField(null=True)
+    codes = models.CharField(max_length= 10)
+    stocks = models.IntegerField()
+    ratings = models.PositiveIntegerField()
+    prices_usd = models.DecimalField(decimal_places= 2, max_digits=10)
+    prices_pen = models.DecimalField(decimal_places= 2, max_digits=10)
+    images = models.URLField(null=True)
+    Fecha = models.DateTimeField(null=True, blank=True)
+    categoria = models.CharField(max_length= 10)
+    empresa = models.CharField(max_length= 10)
+    marca = models.CharField(max_length= 10)
