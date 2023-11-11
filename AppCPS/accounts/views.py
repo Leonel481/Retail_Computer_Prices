866777b662550e84a888c12381b3310b66aadfd9
raini <rmorecueva@gmail.com>
2023-11-11 00:24:41 -0500

    nuevos usuarios pueden agregar comentarios

--- AppCPS/accounts/views.py
+++ AppCPS/accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm)
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+grupo = Group.objects.get(name='userscancomment')
 
 # Create your views here.
 
@@ -10,7 +14,8 @@ def signup_view(request):
 
         if form.is_valid():
             user = form.save()
-
+            usuario = User.objects.get(username=user)
+            usuario.groups.add(grupo)
             login(request, user)
             return redirect('index')
     else:
@@ -23,6 +28,7 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
+
             login(request, user)
             return redirect('index')
 
